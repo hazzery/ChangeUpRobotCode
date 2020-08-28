98b4f43168fc086e16afe97d64b444a645a6082e
Harry Parkes <harrydparkes@protonmail.com>
2020-08-29 11:30:14 +1200

    Basic user controll

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include "main.h"
 #include "setup.h"
 
+using pros::delay;
+
 void initialize() {}
 void disabled() {}
 void competition_initialize() {}
 void autonomous() {}
 
-void opcontrol() {}
+void motorTest() {
+	if(controller.getDigital(ControllerDigital::up)) LFDrive.moveVoltage(12000);
+	else if(controller.getDigital(ControllerDigital::down)) LFDrive.moveVoltage(-12000);
+
+	if(controller.getDigital(ControllerDigital::left)) LBDrive.moveVoltage(12000);
+	else if(controller.getDigital(ControllerDigital::right)) LBDrive.moveVoltage(-12000);
+
+	if(controller.getDigital(ControllerDigital::X)) RFDrive.moveVoltage(12000);
+	else if(controller.getDigital(ControllerDigital::B)) RFDrive.moveVoltage(-12000);
+
+	if(controller.getDigital(ControllerDigital::Y)) RBDrive.moveVoltage(12000);
+	else if(controller.getDigital(ControllerDigital::A)) RBDrive.moveVoltage(-12000);
+}
+
+void opcontrol() {
+	while (true)
+	{
+    	drive->tank(controller.getAnalog(ControllerAnalog::leftY),controller.getAnalog(ControllerAnalog::rightY), 0.3);
+		
+	    delay(100);
+	}
+}
